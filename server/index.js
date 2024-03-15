53c48e5909fec3aad97f75f632d3b9ad0489e26c
Anitej Isaac Sharma <aisaaxs@Anitejs-MacBook-Pro.local>
2024-03-14 17:43:08 -0700

    adding the basic structure for the new SQL database

---
+++ server/index.js
@@ -0,0 +1,88 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Harmony"
+});
+
+app.get('/userdata', (req, res) => {
+    const query = "SELECT * FROM Users";
+    db.query(query, (err, data) => {
+        if(err) return res.status(500).send('Internal Server Error');
+        
+        if(data.length === 0) {
+            return res.send('No data available');
+        }
+        
+        let tableHTML = '<style>' +
+                        'body {' +
+                        '   background-color: black;' +
+                        '}' +
+                        'table {' +
+                        '    width: 100%;' +
+                        '    border-collapse: collapse;' +
+                        '}' +
+                        'th, td {' +
+                        '    border: 1px solid #dddddd;' +
+                        '    padding: 8px;' +
+                        '    text-align: center;' +
+                        '   color: black' +
+                        '}' +
+                        'td {' +
+                        '   color: white;' +
+                        '}' +
+                        'th {' +
+                        '    background-color: #f2f2f2;' +
+                        '   border: 1px solid black;' +
+                        '}' +
+                        '</style>';
+        
+        tableHTML += '<table>';
+        tableHTML += '<tr>';
+        Object.keys(data[0]).forEach(key => {
+            tableHTML += '<th>' + key + '</th>';
+        });
+        tableHTML += '</tr>';
+
+        data.forEach(row => {
+            tableHTML += '<tr>';
+            Object.values(row).forEach(value => {
+                tableHTML += '<td>' + value + '</td>';
+            });
+            tableHTML += '</tr>';
+        });
+
+        tableHTML += '</table>';
+        
+        res.send(tableHTML);
+    });
+});
+
+app.post("/userdata", (req, res) => {
+    const query = "INSERT INTO Users(`name`, `email`, `password`, `username`, `image`) VALUES (?, ?, ?, ?, ?)";
+  
+    const values = [
+      req.body.name,
+      req.body.email,
+      req.body.password,
+      req.body.username,
+      req.body.image
+    ];
+  
+    db.query(query, values, (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  });
+
+app.listen(8800, () => {
+    console.log("connected to server 👾");
+});
