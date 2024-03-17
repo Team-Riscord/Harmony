e51f2b23ac575d869c696507ef1c7d81b65f27fa
Anitej Isaac Sharma <aisaaxs@Anitejs-MacBook-Pro.local>
2024-03-17 02:51:45 -0700

    adding code for the Signup components as well most of the error displaying and input processing functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
