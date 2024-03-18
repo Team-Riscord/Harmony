fb24b92cf46f6f7ffafe4d702799e2deb05eb32c
Anitej Isaac Sharma <aisaaxs@Anitejs-MacBook-Pro.local>
2024-03-18 11:53:50 -0700

    adding minor change

--- client/src/__tests__/Signup.test.js
+++ client/src/__tests__/Signup.test.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Signup from '../components/Signup/Signup';
 
-it('renders without crashing', () => {
-  render(<Signup />);
+describe('Signup Component', () => {
+  test('renders form elements [Signup]', () => {
+    const { getByLabelText, getByText } = render(<Signup />);
+    expect(getByLabelText('enter your full name')).toBeInTheDocument();
+    expect(getByLabelText('enter your email')).toBeInTheDocument();
+    expect(getByLabelText('enter your password')).toBeInTheDocument();
+    expect(getByLabelText('choose a username')).toBeInTheDocument();
+    expect(getByText('sign up')).toBeInTheDocument();
+  });
 });
