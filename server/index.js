d38ba1057dd5909c968785e4cb760cf292956c44
ronakJain21 <ronak15082002@gmail.com>
2024-03-18 15:44:01 -0700

    edit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "[your_password_here]",
+  password: "AnitejPhambytex@8503",
 });
 
 db.connect((err) => {
