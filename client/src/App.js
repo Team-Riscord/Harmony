5747ba956b2c07d392fcb409b690e1a3e1d15cdc
Anitej Isaac Sharma <aisaaxs@Anitejs-MacBook-Pro.local>
2024-03-23 17:50:56 -0700

    adding changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Homepage />} index />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Routes>
