d66f7f1e09d34787a29fdc77407c60e1be43aa60
Anitej Isaac Sharma <aisaaxs@Anitejs-MacBook-Pro.local>
2024-03-24 02:33:32 -0700

    adding changes

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,6 +1,19 @@
 import './Homepage.css';
+import { useNavigate } from 'react-router-dom';
 
 const Homepage = () => {
+    const navigate = useNavigate();
+
+    const goToSignUp = () => {
+        navigate('/signup');
+        navigate('/signup', { replace: true });
+    };
+
+    const goToLogin = () => {
+        navigate('/login');
+        navigate('/login', { replace: true });
+    }
+
     return (
         <div className='homepage-component'>
             <div className='homepage-container'>
@@ -11,10 +24,18 @@ const Homepage = () => {
                 </div>
                 <div className='homepage-description'>
                     <div>
-                        <p><span style={{textDecoration: 'underline'}}>Harmony</span> is a vibrant, user-friendly <span style={{fontStyle: 'italic'}}>Discord</span> clone designed for seamless communication and collaboration. It provides all the essential features needed for chatting, sharing, and connecting with your communities and friends.</p>
+                        <p><span style={{textDecoration: 'underline', color: 'rgb(220, 10, 170)'}}>Harmony</span> is a vibrant, user-friendly <span style={{fontStyle: 'italic', color: 'rgb(68, 15, 198)'}}>Discord</span> clone designed for seamless communication and collaboration. It provides all the essential features needed for chatting, sharing, and connecting with your communities and friends.</p>
                     </div>
                     <div>
-                        <p>Whether you're looking to set up a private server for your friends, join public communities, or simply stay in touch with your teams, Harmony brings the conversation to you, making every interaction memorable.</p>
+                        <p>Whether you're looking to set up a private server for your friends, join public communities, or simply stay in touch with your teams, our instant messaging and VoIP social platform brings the conversation to you, making every interaction memorable.</p>
+                    </div>
+                </div>
+                <div className='homepage-buttons'>
+                    <div className='homepage-button'>
+                        <button onClick={goToSignUp}>Sign Up</button>
+                    </div>
+                    <div className='homepage-button'>
+                        <button onClick={goToLogin}>Login</button>
                     </div>
                 </div>
             </div>
